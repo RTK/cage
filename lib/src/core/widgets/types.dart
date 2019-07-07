485826c7bce496e36e7fbe2a09769876fbe8abda
Rouven <rouven@rouvenkruse.de>
2019-07-07 22:05:15 +0200

    Rename: pubspec.yml -> pubspec.yaml Dartanalyzer: Fix warning Pubspec: Remove pedantic, change homepage + version

--- lib/src/core/widgets/types.dart
+++ lib/src/core/widgets/types.dart
@@ -1,8 +1,9 @@
 // Copyright Rouven T. Kruse. All rights reserved.
 // Use of this source code is governed by an MIT-style license that can be
 // found in the LICENSE file.
+import 'package:flutter/widgets.dart' show VoidCallback;
 
-typedef VoidCallback = void Function();
+export 'package:flutter/widgets.dart' show VoidCallback;
 
 typedef UpdateWidgetCallback = void Function(VoidCallback updateCallback);
 
